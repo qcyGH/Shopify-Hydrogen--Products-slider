3977fbd8f912c20b72e490feafcacf83b75e894a
Max <12maks32@gmail.com>
2023-07-07 19:31:58 +0200

    Change WebComponent to React Component
    Add my ProductCard
    Add swiper styles

--- app/components/ProductSlider.jsx
+++ app/components/ProductSlider.jsx
@@ -1,10 +1,10 @@
-// import function to register Swiper custom elements
-import { register } from 'swiper/element/bundle';
+// import Swiper core and required modules
+import { Navigation, Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import ProductCard from '~/components/ProductCard';
-import { useEffect } from 'react';
 
-register();
 
 /**
  * Display a slider with products and a heading based on some options.
@@ -13,28 +13,41 @@ register();
  */
 export default function ProductSlider({products, header = 'Featured products'}) {
 
-  if (products?.edges?.length === 0) {
+  if (products?.length === 0) {
     return <p>No products found.</p>;
   }
 
   return (
-    <>
-      <h2>{header}</h2>
-      <swiper-container
-        slides-per-view="4"
-        navigation="true"
-        pagination="true"
+    <div className='max-w-[100vw]'>
+      <h2 className='text-2xl font-medium mb-3'>{header}</h2>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2
+          },
+          1024: {
+            slidesPerView: 3
+          },
+          1280: {
+            slidesPerView: 4
+          }
+        }}
       >
         {
-          products?.edges?.map((product) => {
+          products?.map((product) => {
             return (
-              <swiper-slide key={product.node.id}>
-                <ProductCard product={product.node}/>
-              </swiper-slide>
+              <SwiperSlide key={product.id}>
+                <ProductCard product={product}/>
+              </SwiperSlide>
             )
           })
         }
-      </swiper-container>
-    </>
+      </Swiper>
+    </div>
   );
 };
